487217fbb06f22f41cd125d6404e5995fed08545
tlckd <tlckd8581@gmail.com>
2022-05-26 14:54:57 +0900

    2022-05-26 수업

--- sql/user.sql
+++ sql/user.sql
@@ -2,3 +2,12 @@
 
 desc user;
 
+insert into user values(null, '관리자','admin@mysite.com','1234','female',now());
+
+select * from user;
+
+-- login 
+select no,name from user where email='asf' and password='asf';
+
+-- findByNo 
+select no,name,email,gender from user where email='asf' and password='asf';
