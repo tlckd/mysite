16fc737ca30fde6d1fadbb0121babf6f274cbffb
tlckd <tlckd8581@gmail.com>
2022-05-30 11:45:34 +0900

    board #1

---
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/board/BoardActionFactory.java
@@ -0,0 +1,13 @@
+package com.douzone.mysite.web.mvc.board;
+
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class BoardActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		return null;
+	}
+
+}
